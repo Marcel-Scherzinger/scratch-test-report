1ed76363c69f718f009247bbc2a56bb66c9b753e
Marcel Scherzinger <>
2026-06-17 18:26:06 +0200

    report: add Report with Formality aspect

--- src/report.rs
+++ src/report.rs
@@ -8,5 +8,44 @@ use crate::{messages::Messages, simulation::Simulation};
 pub struct Report {
     /// Messages for the whole report
     messages: Messages<Report>,
-    simulation: Simulation,
+    form: Option<Formality>,
+    simulation: Option<Simulation>,
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone, Getters)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Formality {
+    initial_block: Result<svalue::ARc<str>, InitialBlockAmbiguity>,
+    /// if the block pointers form a cycle (true), the file is malicious or at least invalid
+    cyclic_graph: bool,
+}
+
+impl Formality {
+    pub fn new(
+        initial_block: Result<svalue::ARc<str>, InitialBlockAmbiguity>,
+        cyclic_graph: bool,
+    ) -> Self {
+        Self {
+            initial_block,
+            cyclic_graph,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "kebab-case"))]
+pub enum InitialBlockAmbiguity {
+    No,
+    Multiple,
+}
+
+impl Report {
+    pub fn new(form: Option<Formality>, simulation: Option<Simulation>) -> Self {
+        Self {
+            messages: Default::default(),
+            form,
+            simulation,
+        }
+    }
 }

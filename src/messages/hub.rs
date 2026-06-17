4ebcc3ddbad8c487632b0b9414da959358b9bab3
Marcel Scherzinger <>
2026-06-17 22:09:06 +0200

    messages: change struct to tuple struct for inlined serialization of map

--- src/messages/hub.rs
+++ src/messages/hub.rs
@@ -20,10 +20,7 @@ pub trait MessageAdder<Level> {
 /// Collection of multiple messages of a level
 #[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Messages<Level> {
-    #[cfg_attr(feature = "serde", serde(flatten))]
-    messages: BTreeSet<Message<Level>>,
-}
+pub struct Messages<Level>(BTreeSet<Message<Level>>);
 
 impl<Level> Messages<Level> {
     pub fn has_kind(&self, kind: &MessageKind) -> bool {
@@ -33,28 +30,26 @@ impl<Level> Messages<Level> {
 
 impl<Level> Default for Messages<Level> {
     fn default() -> Self {
-        Self {
-            messages: Default::default(),
-        }
+        Self(Default::default())
     }
 }
 
 impl<Level> Messages<Level> {
     pub fn iter(&self) -> std::collections::btree_set::Iter<'_, Message<Level>> {
-        self.messages.iter()
+        self.0.iter()
     }
 }
 
 impl<Level> MessageAdder<Level> for Messages<Level> {
     fn notify(&mut self, msg: Message<Level>) {
-        self.messages.insert(msg);
+        self.0.insert(msg);
     }
 }
 
 impl<Level> Extend<Message<Level>> for Messages<Level> {
     fn extend<T: IntoIterator<Item = Message<Level>>>(&mut self, iter: T) {
         for i in iter {
-            self.messages.insert(i);
+            self.0.insert(i);
         }
     }
 }
@@ -64,7 +59,7 @@ impl<Level> IntoIterator for Messages<Level> {
     type IntoIter = <BTreeSet<Message<Level>> as IntoIterator>::IntoIter;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.messages.into_iter()
+        self.0.into_iter()
     }
 }
 
